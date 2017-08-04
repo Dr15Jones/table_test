e96447572ecefc8429b2c3d65f4302bf877f0604
Chris Jones <chrisdjones15@gmail.com>
2017-08-04 19:29:52 +0000

    Fully build shadow class from TStreamerInfo

--- branch_struct_read_test.cc
+++ branch_struct_read_test.cc
@@ -6,12 +6,59 @@
 #include "TBranchElement.h"
 #include "TStreamerInfo.h"
 #include "TStreamerElement.h"
-#include "TLeaf.h"
 #include "Table.h"
 #include "JetTable.h"
 #include "TInterpreter.h"
 #include <iostream>
 
+bool buildShadowClassFor(TBranch* iBranch) {
+  /* Suggestion from Philippe:
+     cast the top TBranch to TBranchElement and then get the
+     StreamerInfo and use that to get the member data info
+
+     To track which Table type went with which branch, add a
+     comment to the size member data which is the type name.
+     The comment can be obtained again via the GetTitle for the
+     appropriate TStreamInfo branch entry for 'size'
+   */
+
+  auto be = dynamic_cast<TBranchElement*>(iBranch);
+  assert(be != nullptr);
+  auto si = be->GetInfo();
+
+  std::string className = si->GetName();
+  size_t pos;
+  int nNamespaces = 0;
+
+  std::string strct;
+  while(std::string::npos != (pos = className.find_first_of(':')) ) {
+    ++nNamespaces;
+    strct +="namespace ";
+    strct+=className.substr(0,pos)+" { ";
+    className = className.substr(pos+2);
+  }
+  strct +=" struct ";
+  strct += className +" {\n";
+
+  for(int i = 0; i< si->GetNelement();++i) {
+    auto e = si->GetElement(i);
+    strct +=" ";
+    strct += e->GetTypeName();
+    strct +=" ";
+    strct += e->GetName();
+    strct += ";//";
+    strct +=e->GetTitle();
+    strct +="\n";
+  }
+  strct +="};";
+  while(nNamespaces > 0) {
+    --nNamespaces;
+    strct +="}";
+  }
+  std::cout <<strct<<std::endl;
+
+  return gInterpreter->Declare(strct.c_str());
+}
 
 int main()
 {
@@ -26,52 +73,14 @@ int main()
   JetTable jets;
   JetTable* pJets = &jets;
 
-  //std::cout <<"size "<<jets.size()<<std::endl;
-
-  std::string strct = "namespace bw { struct ";
 
   const std::string branchName("Jets.");  
   auto jetBranch = tree->GetBranch(branchName.c_str());
-  {
-    auto l = static_cast<TLeaf*>(jetBranch->GetListOfLeaves()->At(0));
-    strct += l->GetTypeName()+4;
-    strct += " {\n";
-  }
-
-  /* Suggestion from Philippe:
-     cast the top TBranch to TBranchElement and then get the
-     StreamerInfo and use that to get the member data info
-
-     To track which Table type went with which branch, add a
-     comment to the size member data which is the type name.
-     The comment can be obtained again via the GetTitle for the
-     appropriate TStreamInfo branch entry for 'size'
-   */
-  {
-    auto je = dynamic_cast<TBranchElement*>(jetBranch);
-    assert(je != nullptr);
-    auto si = je->GetInfo();
-    for(int i = 0; i< si->GetNelement();++i) {
-      auto e = si->GetElement(i);
-      strct +=" ";
-      strct += e->GetTypeName();
-      strct +=" ";
-      strct += e->GetName();
-      strct += ";//";
-      strct +=e->GetTitle();
-      strct +="\n";
-    }
-  }
-
-  strct += "}; }";
-  std::cout <<strct<<std::endl;
-  gInterpreter->Declare(strct.c_str());
-
   assert(0 != jetBranch);
+  buildShadowClassFor(jetBranch);
+
   jetBranch->SetAddress(&pJets);
   
-  std::cout <<"size "<<jets.size()<<std::endl;
-
   auto nEntries = tree->GetEntries();
   for(decltype(nEntries) i = 0; i< nEntries; ++i) {
     
@@ -84,9 +93,3 @@ int main()
   
   return 0;
 }
-
-/*
-build doing
-
-g++ -std=c++14 -I $ROOTSYS/include branch_test.cc -L $ROOTSYS/lib -l RIO -l Tree -l Core
-*/
